bfd89cee5157dfa9970c3124e1503c78d90ab210
Nikita Vinogradov <n.a.vinogradov94@gmail.com>
2023-01-20 06:24:16 +0300

    switching to firebase

---
+++ src/firebase-config.ts
@@ -0,0 +1,15 @@
+import { initializeApp} from 'firebase/app'
+import { getFirestore } from '@firebase/firestore'
+import { initializeFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_projectId,
+  storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+  appId: process.env.REACT_APP_FIREBASE_appId
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
--- src/pages/login-page/login.tsx
+++ src/pages/login-page/login.tsx
@@ -1,5 +1,20 @@
 import styles from './login.module.scss';
+import { db } from '../../firebase-config';
+import { collection, doc, getDocs } from '@firebase/firestore';
 
 export const LoginPage = () => {
-  return <div>Страница логина</div>;
+  const usersCollection = collection(db, 'users');
+  const handleClick = async () => {
+    const getUser = async () => {
+      const data = await getDocs(usersCollection);
+      return data.docs.map((el) => ({ ...el.data(), id: el.id }));
+    };
+    console.log(await getUser());
+  };
+  return (
+    <>
+      <div>Страница логина</div>
+      <button onClick={handleClick}>Test</button>
+    </>
+  );
 };
